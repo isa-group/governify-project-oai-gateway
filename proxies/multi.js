dc5ef584eb26c28f21f8a95dc4c47a3c9b2597ce
Daniel Arteaga <darteaga@us.es>
2016-11-04 11:49:53 +0100

    instruciones

--- proxies/multi.js
+++ proxies/multi.js
@@ -1,67 +1,72 @@
 'use strict';
 
-var request   = require('request'),
-    http      = require('http'),
-    services  = require('../database').data.services;
+var request = require('request'),
+    http = require('http'),
+    services = require('../database').data.services;
 
 var config = require('../config'),
     logger = config.logger;
 
 module.exports = function (req, res, next) {
     //(req.originalUrl.indexOf('docs') !== -1 && req.headers['referer'] && req.headers['referer'].indexOf('plans') === -1)
-    if( false || req.originalUrl.indexOf('gateway') !== -1 ) return next();
+    if (false || req.originalUrl.indexOf('gateway') !== -1) return next();
 
     logger.info("Referer from: %s as service: %s ",
-      req.headers['referer'], (req.headers['referer'] ? req.headers['referer'].split('/')[3] : 'No referer'));
+        req.headers['referer'], (req.headers['referer'] ? req.headers['referer'].split('/')[3] : 'No referer'));
 
-    logger.info("Proxing internal %s ...", req.originalUrl );
+    logger.info("Proxing internal %s ...", req.originalUrl);
     var servicePath = req.originalUrl.split('/')[1];
     var serviceInfo = services[servicePath] ||
-                      services[(req.headers['referer'] ? req.headers['referer'].split('/')[3] : 'No referer')];
+        services[(req.headers['referer'] ? req.headers['referer'].split('/')[3] : 'No referer')];
 
-    if(!serviceInfo){
+    if (!serviceInfo) {
         logger.info('There is not service registered for name %s', servicePath);
         return res.status(405).end("Method Not Allowed");
     }
 
-    try{
-      var proxiedServer = {
-        hostname: 'localhost',
-        port: serviceInfo.port,
-        path: req.originalUrl.replace('/'+serviceInfo.name, '')
-      }
-
-      logger.info("Sending to: %s",  JSON.stringify(proxiedServer));
-      res.setHeader("host", req.headers.host);
-      var proxiedRequest = http.request(proxiedServer, function(response){
-        if(response.statusCode === 404){
-            return next();
+    try {
+        var proxiedServer = {
+            hostname: 'localhost',
+            port: serviceInfo.port,
+            path: req.originalUrl.replace('/' + serviceInfo.name, '')
         }
 
-        for (var h in response.headers){
-            res.setHeader(h, response.headers[h]);
-        }
+        logger.info("Sending to: %s", JSON.stringify(proxiedServer));
+        res.setHeader("host", req.headers.host);
 
-        logger.info('Status from proxied server: %s' , response.statusCode);
-        if (response.statusCode === 302 || response.statusCode === 301 ) {
-            logger.info('Redirecting: %s' , '/' + serviceInfo.name + response.headers['location']);
-            res.redirect('/' + serviceInfo.name + response.headers['location']);
-        }else{
-            logger.info("Piping response...");
-            response.pipe(res);
-        }
+        //crear un objeto que contiene todas las cabeceras de la request (req)
+        //y añadir a la opciones del
+        //setear la opcion para https
 
-      }).on('error', (err, ress) => {
-          logger.info(err, ress);
-          res.status(500).end(err.toString());
-      });
+        var proxiedRequest = http.request(proxiedServer, function (response) {
+            if (response.statusCode === 404) {
+                return next();
+            }
 
-      req.pipe(proxiedRequest);
+            for (var h in response.headers) {
+                res.setHeader(h, response.headers[h]);
+            }
 
-    }catch(e){
+            logger.info('Status from proxied server: %s', response.statusCode);
+            if (response.statusCode === 302 || response.statusCode === 301) {
+                logger.info('Redirecting: %s', '/' + serviceInfo.name + response.headers['location']);
+                res.redirect('/' + serviceInfo.name + response.headers['location']);
+            } else {
+                logger.info("Piping response...");
+                response.pipe(res);
+            }
 
-      res.status(503)
-      res.send("Proxied server unreachable:" + e);
+        }).on('error', (err, ress) => {
+            logger.info(err, ress);
+            res.status(500).end(err.toString());
+        });
+
+        req.pipe(proxiedRequest);
+
+    } catch (e) {
+
+        res.status(503)
+        res.send("Proxied server unreachable:" + e);
 
     }
 
--- proxies/single.js
+++ proxies/single.js
@@ -1,38 +1,46 @@
 'use strict';
 
-var request   = require('request'),
-    http      = require('http'),
-    url       = require('url'),
-    services  = require('../database').data.services;
+var request = require('request'),
+    http = require('http'),
+    url = require('url'),
+    services = require('../database').data.services;
 
-var config    = require('../config'),
-    services  = require('../database').data.services,
-    logger    = config.logger;
+var config = require('../config'),
+    services = require('../database').data.services,
+    logger = config.logger;
 
 module.exports = function (req, res, next) {
 
-  if( req.originalUrl.indexOf('plans') !== -1 || req.originalUrl.indexOf('docs') !== -1 ) return next();
+    if (req.originalUrl.indexOf('plans') !== -1 || req.originalUrl.indexOf('docs') !== -1) return next();
 
-  var proxiedServer = services[req.serviceProxied].url;
-  var path = req.originalUrl;
+    var proxiedServer = services[req.serviceProxied].url;
+    var path = req.originalUrl;
 
-  logger.info("Sending to server: " + proxiedServer + path + "...");
+    logger.info("Sending to server: " + proxiedServer + path + "...");
 
-  try {
-      var renameHost = services[req.serviceProxied].url.split('//')[1];
-      res.setHeader("host", renameHost);
-      req.pipe(request({ url: proxiedServer + path, headers: { host: renameHost } }, function (err, response) {
-        if (err) {
-          res.sendStatus(503);
-        }
-        logger.info("Response from server: " + response.statusCode);
-      })).pipe(res);
+    try {
+        var renameHost = services[req.serviceProxied].url.split('//')[1];
+        res.setHeader("host", renameHost);
+        //construir un objecto con todas las cabeceras del request (req) y pasar
+        //en las opciones del nuevo request
+        //setear la opcion para https
+        req.pipe(request({
+            url: proxiedServer + path,
+            headers: {
+                host: renameHost
+            }
+        }, function (err, response) {
+            if (err) {
+                res.sendStatus(503);
+            }
+            logger.info("Response from server: " + response.statusCode);
+        })).pipe(res);
 
-  } catch (e) {
+    } catch (e) {
 
-      res.status(503)
-      res.send("Proxied server unreachable:" + e);
+        res.status(503)
+        res.send("Proxied server unreachable:" + e);
 
-  }
+    }
 
 }
